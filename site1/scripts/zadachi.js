ebf2672f049d285bf6d9166fbd8d9ea50cf8c33d
kodywin <egorkarpenko2000@gmail.com>
2017-05-07 23:39:52 +0300

    vse dobavil

---
+++ site1/scripts/zadachi.js
@@ -0,0 +1,56 @@
+function zadacha1()
+{
+    var element = document.getElementById('zadacha1');
+    element.innerHTML = '<img src= "image/f01.jpg">';
+}
+function zadacha2()
+{
+    var element = document.getElementById('zadacha2');
+    element.innerHTML = '<img src= "image/f02.jpg">';
+}
+function zadacha3()
+{
+    var element = document.getElementById('zadacha3');
+    element.innerHTML = '<img src= "image/f03.jpg">';
+}
+function zadacha4()
+{
+    var element = document.getElementById('zadacha4');
+    element.innerHTML = '<img src= "image/f04.jpg">';
+}
+function zadacha5()
+{
+    var element = document.getElementById('zadacha5');
+    element.innerHTML = '<img src= "image/f05.jpg">';
+}
+function zadacha6()
+{
+    var element = document.getElementById('zadacha6');
+    element.innerHTML = '<img src= "image/f06.jpg">';
+}
+function zadacha7()
+{
+    var element = document.getElementById('zadacha7');
+    element.innerHTML = '<img src= "image/f07.jpg">';
+}
+function zadacha8()
+{
+    var element = document.getElementById('zadacha8');
+    element.innerHTML = '<img src= "image/f08.jpg">';
+}
+function zadacha9()
+{
+    var element = document.getElementById('zadacha9');
+    element.innerHTML = '<img src= "image/f09.jpg">';
+}function zadacha10()
+{
+    var element = document.getElementById('zadacha10');
+    element.innerHTML = '<img src= "image/f10.jpg">';
+}
+function zadacha11()
+{
+    var element = document.getElementById('zadacha11');
+    element.innerHTML = '<img src= "image/f11.jpg">';
+}
+
+
