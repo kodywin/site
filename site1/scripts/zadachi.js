b14be990041297268a53b2d04659e18fbfd8de09
kodywin <egorkarpenko2000@gmail.com>
2017-05-08 10:01:07 +0300

    zadachi

--- site1/scripts/zadachi.js
+++ site1/scripts/zadachi.js
@@ -3,56 +3,77 @@ function zadacha1()
     var element = document.getElementById('zadacha1');
     var container = document.getElementById('cont');
     element.innerHTML = '<img src= "image/f01.jpg">';
-    container.innerHTML = '<img src= "image/z01.jpg">'
+    container.innerHTML = '<img class="z01" src= "image/z01.jpg">'
 }
 function zadacha2()
 {
     var element = document.getElementById('zadacha2');
+    var container = document.getElementById('cont');
     element.innerHTML = '<img src= "image/f02.jpg">';
+    container.innerHTML = '<img class="z01" src= "image/z02.jpg">'
 }
 function zadacha3()
 {
     var element = document.getElementById('zadacha3');
+    var container = document.getElementById('cont');
     element.innerHTML = '<img src= "image/f03.jpg">';
+    container.innerHTML = '<img class="z01" src= "image/z03.jpg">'
 }
 function zadacha4()
 {
     var element = document.getElementById('zadacha4');
+    var container = document.getElementById('cont');
     element.innerHTML = '<img src= "image/f04.jpg">';
+    container.innerHTML = '<img class="z01" src= "image/z04.jpg">'
 }
 function zadacha5()
 {
     var element = document.getElementById('zadacha5');
-    element.innerHTML = '<img src= "image/f05.jpg">';
+    var container = document.getElementById('cont');
+    element.innerHTML = '<img class="f05" src= "image/f05.jpg">';
+    container.innerHTML = '<img class="z01" src= "image/z05.jpg">'
 }
 function zadacha6()
 {
     var element = document.getElementById('zadacha6');
-    element.innerHTML = '<img src= "image/f06.jpg">';
+    var container = document.getElementById('cont');
+    element.innerHTML = '<img class="f05" src= "image/f06.jpg">';
+    container.innerHTML = '<img class="z01" src= "image/z06.jpg">'
 }
 function zadacha7()
 {
     var element = document.getElementById('zadacha7');
-    element.innerHTML = '<img src= "image/f07.jpg">';
+    var container = document.getElementById('cont');
+    element.innerHTML = '<img class="f05" src= "image/f07.jpg">';
+    container.innerHTML = '<img class="z01" src= "image/z07.jpg">'
 }
 function zadacha8()
 {
     var element = document.getElementById('zadacha8');
+    var container = document.getElementById('cont');
     element.innerHTML = '<img src= "image/f08.jpg">';
+    container.innerHTML = '<img class="z01" src= "image/z08.jpg">'
 }
 function zadacha9()
 {
     var element = document.getElementById('zadacha9');
+    var container = document.getElementById('cont');
     element.innerHTML = '<img src= "image/f09.jpg">';
-}function zadacha10()
+    container.innerHTML = '<img class="z01" src= "image/z09.jpg">'
+}
+function zadacha10()
 {
     var element = document.getElementById('zadacha10');
+    var container = document.getElementById('cont');
     element.innerHTML = '<img src= "image/f10.jpg">';
+    container.innerHTML = '<img class="z01" src= "image/z10.jpg">'
 }
 function zadacha11()
 {
     var element = document.getElementById('zadacha11');
-    element.innerHTML = '<img src= "image/f11.jpg">';
+    var container = document.getElementById('cont');
+    element.innerHTML = '<img class="f05" src= "image/f11.jpg">';
+    container.innerHTML = '<img class="z01" src= "image/z11.jpg">'
 }
 
 
