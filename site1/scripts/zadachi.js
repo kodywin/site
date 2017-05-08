a9cd40b75b83286a686c5f947a55a54e7e59c816
egoist301 <filippovich.z@mail.ru>
2017-05-08 13:32:37 +0400

    s

--- site1/scripts/zadachi.js
+++ site1/scripts/zadachi.js
@@ -3,28 +3,28 @@ function zadacha1()
     var element = document.getElementById('zadacha1');
     var container = document.getElementById('cont');
     element.innerHTML = '<img class ="f01" src= "image/f01.jpg">';
-    container.innerHTML = '<img class="z01" src= "image/z01.jpg">'
+    container.innerHTML = '<img class="z01" src= "image/z01.jpg">';
 }
 function zadacha2()
 {
     var element = document.getElementById('zadacha2');
     var container = document.getElementById('cont');
     element.innerHTML = '<img class ="f02" src= "image/f02.jpg">';
-    container.innerHTML = '<img class="z01" src= "image/z02.jpg">'
+    container.innerHTML = '<img class="z01" src= "image/z02.jpg">';
 }
 function zadacha3()
 {
     var element = document.getElementById('zadacha3');
     var container = document.getElementById('cont');
     element.innerHTML = '<img class ="f03" src= "image/f03.jpg">';
-    container.innerHTML = '<img class="z03" src= "image/z03.jpg">'
+    container.innerHTML = '<img class="z03" src= "image/z03.jpg">';
 }
 function zadacha4()
 {
     var element = document.getElementById('zadacha4');
     var container = document.getElementById('cont');
     element.innerHTML = '<img class ="f04" src= "image/f04.jpg">';
-    container.innerHTML = '<img class="z01" src= "image/z04.jpg">'
+    container.innerHTML = '<img class="z01" src= "image/z04.jpg">';
 }
 function zadacha5()
 {
@@ -38,21 +38,21 @@ function zadacha6()
     var element = document.getElementById('zadacha6');
     var container = document.getElementById('cont');
     element.innerHTML = '<img class ="f06" class="f05" src= "image/f06.jpg">';
-    container.innerHTML = '<img class="z01" src= "image/z06.jpg">'
+    container.innerHTML = '<img class="z01" src= "image/z06.jpg">';
 }
 function zadacha7()
 {
     var element = document.getElementById('zadacha7');
     var container = document.getElementById('cont');
     element.innerHTML = '<img class ="f07" src= "image/f07.jpg">';
-    container.innerHTML = '<img class="z01" src= "image/z07.jpg">'
+    container.innerHTML = '<img class="z01" src= "image/z07.jpg">';
 }
 function zadacha8()
 {
     var element = document.getElementById('zadacha8');
     var container = document.getElementById('cont');
     element.innerHTML = '<img class ="f08" src= "image/f08.jpg">';
-    container.innerHTML = '<img class="z01" src= "image/z08.jpg">'
+    container.innerHTML = '<img class="z01" src= "image/z08.jpg">';
 }
 function zadacha9()
 {
@@ -66,15 +66,41 @@ function zadacha10()
     var element = document.getElementById('zadacha10');
     var container = document.getElementById('cont');
     element.innerHTML = '<img class ="f10" src= "image/f10.jpg">';
-    container.innerHTML = '<img class="z01" src= "image/z10.jpg">'
+    container.innerHTML = '<img class="z01" src= "image/z10.jpg">';
 }
 function zadacha11()
 {
     var element = document.getElementById('zadacha11');
     var container = document.getElementById('cont');
     element.innerHTML = '<img class ="f11" class="f05" src= "image/f11.jpg">';
-    container.innerHTML = '<img class="z01" src= "image/z11.jpg">'
+    container.innerHTML = '<img class="z01" src= "image/z11.jpg">';
 }
-
+function ob()
+{
+    var container = document.getElementById('cont');
+    container.innerHTML = '';
+    var element = document.getElementById('zadacha10');
+    element.innerHTML = 'Равновесие невращающихся тел';
+    var element = document.getElementById('zadacha11');
+    element.innerHTML = 'Центр масс. Центр тяжести';
+    var element = document.getElementById('zadacha9');
+    element.innerHTML = 'Движение спутников под действием сил тяготения';
+    var element = document.getElementById('zadacha8');
+    element.innerHTML = 'Вес тела';
+    var element = document.getElementById('zadacha7');
+    element.innerHTML = 'Движение тела, брошенного горизонтально под углом к горизонту';
+    var element = document.getElementById('zadacha6');
+    element.innerHTML = 'Силы трения скольжения, вязкого трения';
+    var element = document.getElementById('zadacha5');
+    element.innerHTML = 'Силы трения покоя';
+    var element = document.getElementById('zadacha4');
+    element.innerHTML = 'Закон всемирного тяготения';
+    var element = document.getElementById('zadacha3');
+    element.innerHTML = 'Закон Гука';
+    var element = document.getElementById('zadacha2');
+    element.innerHTML = 'Второй закон Ньютона для сил, действующих в пространстве(на плоскости)';
+    var element = document.getElementById('zadacha1');
+    element.innerHTML = 'Второй закон Ньютона для нескольких сил, действующих вдоль одной прямой';
+}
 
 
